0edc48d4d7fa989f2303a209101cc94e74a056a6
Pro-input <omar.a@input.ae>
2022-06-19 12:37:50 +0400

    add instrumentationKey in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   appInsights:{
-    instrumentationKey:'f21a6d6d-69b3-429c-8cdd-47a7ec9b6a10'
+    instrumentationKey:'277c1a22-6d08-47f2-9b4a-39783d5f5bc5'
   }
 };
